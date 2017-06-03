61b32ccc5b2585572f8db1b9c62732f91be88351
systart <etzcuoglu@gmail.com>
2017-06-03 15:07:46 +0300

    First commit.

---
+++ MatrixExpander/MatrixOperations.cs
@@ -0,0 +1,22 @@
+﻿namespace MatrixExpander
+{
+    public static class MatrixOperations
+    {
+        public static int[,] Expand(int multiplier, int[,] matrix)
+        {
+            int newN = matrix.GetLength(0) * multiplier;
+            int[,] tmpMatrix = new int[newN, newN];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int column = 0; column < matrix.GetLength(1); column++)
+                {
+                    tmpMatrix[(row * multiplier), (column * multiplier)] = matrix[row, column];
+                    tmpMatrix[(row * multiplier), ((column * multiplier) + 1)] = matrix[row, column];
+                    tmpMatrix[(row * multiplier) + 1, (column * multiplier)] = matrix[row, column];
+                    tmpMatrix[(row * multiplier) + 1, ((column * multiplier) + 1)] = matrix[row, column];
+                }
+            }
+            return tmpMatrix;
+        }
+    }
+}
---
+++ MatrixExpander/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MatrixExpander
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int multiplier = 0;
+            Console.Write("N: ");
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Please enter a number.");
+            }
+            int[,] matrix = new int[n, n];
+            Console.Write("Multiplier: ");
+            while (!int.TryParse(Console.ReadLine(), out multiplier))
+            {
+                Console.WriteLine("Please enter a number.");
+            }
+            int newN = n * multiplier;
+            int[,] newMatrix = new int[newN, newN];
+            Console.WriteLine("\nInput: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("Matrix[" + i + "][" + j + "]: ");
+                    while (!int.TryParse(Console.ReadLine(), out matrix[i, j]))
+                    {
+                        Console.WriteLine("Please enter a number.");
+                    }
+                }
+            }
+            newMatrix = MatrixOperations.Expand(multiplier, matrix);
+            Console.WriteLine("\nResult:");
+            for (int i = 0; i < newN; i++)
+            {
+                for (int j = 0; j < newN; j++)
+                {
+                    Console.Write(newMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
